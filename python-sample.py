2397e1f9e83eee4f689f782bfcfb47588145c27d
Steven DiCarlo <stevendicarlo2@gmail.com>
2017-08-03 15:13:15 -0400

    Update python-sample.py

--- python-sample.py
+++ python-sample.py
@@ -18,10 +18,27 @@ def wombat(state, time_left):
             return itemAt(4, 3)
         if direction == 's':
             return itemAt(3, 4)
+        
+    def atEdge():
+        x = state['global-coords']
+    
+    def facingEdgeOfScreen():
+        width = state['global-dimensions][0] - 1
+        height = state['global-dimensions][1] - 1
+        if direction == 'w':
+            return state['global-coords'][0] == 0
+        if direction == 'n':
+            return state['global-coords'][1] == 0
+        if direction == 'e':
+            return state['global-coords'][0] == width
+        if direction == 's':
+            return state['global-coords'][1] == height        
     
     if itemInFront() in ['wombat', 'zakano', 'wood-barrier']:
         command = shoot
     elif itemInFront() in ['steel-barrier', 'poison', 'shot', 'fog']:
+        command = turnLeft
+    elif facingEdgeOfScreen():
         command = turnLeft
     elif itemInFront() in ['food', 'open']:
         command = move
@@ -33,5 +50,5 @@ def wombat(state, time_left):
     # Note that the function name MUST be wombat
     return {
         'command': command,
-        'state': direction
+        'state': state
     }
