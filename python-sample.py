139ff659740aa71307f4e9aa6bd9d2d42829c3b5
Steven DiCarlo <stevendicarlo2@gmail.com>
2017-08-03 14:45:25 -0400

    Update python-sample.py

--- python-sample.py
+++ python-sample.py
@@ -3,15 +3,34 @@ def wombat(state, time_left):
     turnRight = {'action': 'turn', 'metadata': { 'direction': 'right'} } 
     shoot = {'action': 'shoot', 'metadata': { } }
     move = {'action': 'move', 'metadata': { } }
-    #turnLeft = {'action': 'turn', 'metadata': { 'direction': 'left'} }
+    direction = state['arena'][3][3]['orientation']
     
-    command = turnLeft
+    
+    def itemAt(x, y):
+        return state['arena'][y][x]
+    
+    def itemInFront():
+        if direction == w:
+            return itemAt(2, 3)
+        if direction == n:
+            return itemAt(3, 2)
+        if direction == e:
+            return itemAt(4, 3)
+        if direction == s:
+            return itemAt(3, 4)
+    
+    if itemInFront in ['wombat', 'zakano', 'wood-barrier']:
+        command = shoot
+    if itemInFront in ['steel-barrier', 'poison', 'shot', 'fog']:
+        command = turnLeft
+    if itemInFront in ['food', 'open']:
+        command = move
+    
+    
     
     
     # Note that the function name MUST be wombat
     return {
         'command': command,
-        'state': {
-            'hello': 'world'
-        }
+        'state': state
     }
