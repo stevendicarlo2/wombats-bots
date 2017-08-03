60f8357f5af03d1ac85104ce8eb48fd6915cc6dc
Steven DiCarlo <stevendicarlo2@gmail.com>
2017-08-03 14:57:33 -0400

    Update python-sample.py

--- python-sample.py
+++ python-sample.py
@@ -3,7 +3,7 @@ def wombat(state, time_left):
     turnRight = {'action': 'turn', 'metadata': { 'direction': 'right'} } 
     shoot = {'action': 'shoot', 'metadata': { } }
     move = {'action': 'move', 'metadata': { } }
-    direction = state['arena'][3][3]['contents']['orientation']
+    direction = state['arena'][3][3]['contents']['orientation']['contents']['type']
     
     
     def itemAt(x, y):
