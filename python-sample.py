add0424261fd56a90fdadaf3cdb4fc3f10f68882
Steven DiCarlo <stevendicarlo2@gmail.com>
2017-08-03 14:33:01 -0400

    Update python-sample.py

--- python-sample.py
+++ python-sample.py
@@ -1,12 +1,16 @@
 def wombat(state, time_left):
+    turnLeft = {'action': 'turn', 'metadata': { 'direction': 'left'} }
+    turnRight = {'action': 'turn', 'metadata': { 'direction': 'right'} } 
+    shoot = {'action': 'shoot', 'metadata': { } }
+    move = {'action': 'move', 'metadata': { } }
+    #turnLeft = {'action': 'turn', 'metadata': { 'direction': 'left'} }
+    
+    command = turnLeft
+    
+    
     # Note that the function name MUST be wombat
     return {
-        'command': {
-            'action': 'turn',
-            'metadata': {
-                'direction': 'right'
-            }
-        },
+        'command': command,
         'state': {
             'hello': 'world'
         }
